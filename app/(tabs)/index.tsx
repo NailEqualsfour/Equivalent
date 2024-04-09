8df8a11ea9e0496368b2bd58938a463e28cc2bc3
NailEqualsfour <SkylerAtTheDoor@gmail.com>
2024-04-10 03:30:16 +0800

    Implement new device detection. Create new account at database if new device detected.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter } from 'expo-router';
+import Constants from 'expo-constants';
 
 import TestComponent from '@/components/TestComponent';
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc, doc, getDocs, query, where } from "firebase/firestore"; 
+
 
 export default function HomeScreen() {
   const router = useRouter();
@@ -18,10 +20,19 @@ export default function HomeScreen() {
     messagingSenderId: "519696329551",
     appId: "1:519696329551:android:7e64512476ab29cc49f33a"
   };
-
   const app = initializeApp(firebaseConfig);
 
-  addDoc(collection(getFirestore(app), 'accounts'), { ip_address: 'yesnt'});
+  async function getAccountById(deviceId: string) {
+    const document = await getDocs(query(collection(getFirestore(app), 'accounts'), where('account_id', '==', deviceId)));
+
+    if (!document.empty) {
+          console.log(document.docs[0].data());
+    } else {
+      addDoc(collection(getFirestore(app), 'accounts'), { account_id: Constants.installationId});
+      console.log('New Phone Detected. New Account Created.');
+    }
+  }
+  getAccountById(Constants.installationId)
 
   return (
     <SafeAreaView style={styles.container}>
