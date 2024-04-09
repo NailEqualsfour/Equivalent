411e9f3903954d22a1eebed27559459db0a917f8
NailEqualsfour <SkylerAtTheDoor@gmail.com>
2024-04-10 02:26:32 +0800

    Initialised Firestore database.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,34 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import TestComponent from '@/components/TestComponent';
 
-export default function TabOneScreen() {
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore"; 
+
+export default function HomeScreen() {
+  const router = useRouter();
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyCc0QS52_4frVNHMczlvPpd92aFk7NFzIA",
+    authDomain: "equivalent-c525f.firebaseapp.com",
+    projectId: "equivalent-c525f",
+    storageBucket: "equivalent-c525f.appspot.com",
+    messagingSenderId: "519696329551",
+    appId: "1:519696329551:android:7e64512476ab29cc49f33a"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  addDoc(collection(getFirestore(app), 'accounts'), { ip_address: 'yesnt'});
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Test of thy test</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titl}>Test of thy testtrs</Text>
+
+      <TestComponent/>
+    </SafeAreaView>
   );
 }
 
@@ -20,7 +39,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   title: {
-    fontSize: 20,
+    fontSize: 0,
     fontWeight: 'bold',
   },
   separator: {
---
+++ components/TestComponent.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function TestComponent() {
+  return (
+    <View>
+      <Text>yay component here u lil shet</Text>
+    </View>
+  );
+}
